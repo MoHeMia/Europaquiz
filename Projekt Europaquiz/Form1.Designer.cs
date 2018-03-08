a4222d56445703bd19c81fb75e9fea1360d4d5fe
schule <schule@R103-1012>
2018-03-08 09:46:09 +0100

    update 0.3.5

--- Projekt Europaquiz/Form1.Designer.cs
+++ Projekt Europaquiz/Form1.Designer.cs
@@ -88,6 +88,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
